3afdae9d4d01e42eeb29b1757b86c6ac52049298
SlaverChief <uvaysov.2005@mail.ru>
2023-06-07 14:55:13 +0400

    'AddRedirection'

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def main(request):
+    return HttpResponse('Hi dude')
+
+
+def showrooms(request):
+    return HttpResponse('Here must be rooms')
